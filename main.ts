5fece774a18775b68c8047244cd7ddbe297ea457
cspanjian <58835905+cspanjian@users.noreply.github.com>
2023-04-10 10:41:17 +0800

    优化了系统性能：把读UID的命令直接准备好，同时扩展了写入字符串的最大长度，现在用6个数据块，可以容纳最多30个中文字符

--- main.ts
+++ main.ts
@@ -7,18 +7,13 @@
 namespace NFC {
     let myNFCevent: Action = null;
     let receivedLen = 0;
-    let password = pins.createBuffer(6);
     let receivedBuffer = pins.createBuffer(25);
     let uid = pins.createBuffer(4);
     let myRxPin=SerialPin.P14;
     let myTxPin=SerialPin.P13;
     let init=false;
-    password[0] = 0xFF;
-    password[1] = 0xFF;
-    password[2] = 0xFF;
-    password[3] = 0xFF;
-    password[4] = 0xFF;
-    password[5] = 0xFF;
+    //反正一直要用读取UID的cmd的，这里直接生成好存下来。直接把校验和填入，省去计算的时间
+    let cmdUID = pins.createBufferFromArray([0x01, 0x08, 0xA1, 0x20, 0x00, 0x01, 0x00, -138])
 
     //% advanced=true shim=NFC::RxBufferedSize
     function RxBufferedSize(): number {
@@ -71,12 +66,7 @@ namespace NFC {
     //% blockId="getUID" block="RFID UID string"
     export function getUID(): string {
         serial.setRxBufferSize(50)
-     //   wakeup();
-        let myBuffer: number[] = []
         let uidBuffer: number[] = []
-        myBuffer = [0x01, 0x08, 0xA1, 0x20, 0x00, 0x01, 0x00, 0x00]
-        fillInCheckSum(myBuffer);
-        let cmdUID = pins.createBufferFromArray(myBuffer)
         serial.writeBuffer(cmdUID);
         basic.pause(50);
         receivedLen = RxBufferedSize();
@@ -94,7 +84,7 @@ namespace NFC {
     }
 
      //% weight=70
-    //% blockId="cardInitialised" block="Initialised RFID reader?"
+    //% blockId="cardInitialised" block="Initialised RFID module?"
     export function cardInitialised(): boolean {
         return init;
       }
@@ -103,12 +93,6 @@ namespace NFC {
     //% blockId="detectedRFIDcard" block="Detected RFID card?"
     export function detectedRFIDcard(): boolean {
         serial.setRxBufferSize(50)
-        //   wakeup();
-        let myBuffer: number[] = []
-        let uidBuffer: number[] = []
-        myBuffer = [0x01, 0x08, 0xA1, 0x20, 0x00, 0x01, 0x00, 0x00]
-        fillInCheckSum(myBuffer);
-        let cmdUID = pins.createBufferFromArray(myBuffer)
         serial.writeBuffer(cmdUID);
         basic.pause(50);
         receivedLen = RxBufferedSize();
@@ -121,13 +105,13 @@ namespace NFC {
 
     //% weight=70
     //% myText
-    //% blockId="writeStringToCard" block="Write string %myText to card"
+    //% blockId="writeStringToCard" block="write string %myText to card"
     export function writeStringToCard(myText: string): boolean {
         return sendStringToCard(myText);
     }
 
     //% weight=70
-    //% blockId="readStringFromCard" block="Read string from card"
+    //% blockId="readStringFromCard" block="read string from card"
     export function readStringFromCard(): string {
         return getStringFromCard();
     }
@@ -177,10 +161,10 @@ namespace NFC {
     }
 
     /**
-     * 把字符串写入到RFID卡里，最多只用3个block，一共48-2=46个byte，所以最少是11个字符
+     * 把字符串写入到RFID卡里，最多只用6个block，一共96-2=94个byte，所以最少是31个字符（UTF-8格式）
      * 写入的字符串，末尾最后固定加上连续的2个0x00，从而让程序知道到了数据的尾部
-     * 如果传入的字符串超过46个byte，则截断。截断的时候，最后未必有连续的2个0x00了
-     * 数据块固定使用 0x04, 0x05, 0x06
+     * 如果传入的字符串超过94个byte，则截断。截断的时候，最后未必有连续的2个0x00了
+     * 数据块固定使用 0x04, 0x05, 0x06, 0x08, 0x09, 0x0A
      */
     function sendStringToCard(myText: string): boolean{
         let byteArr = Buffer.fromUTF8(myText);
@@ -188,12 +172,16 @@ namespace NFC {
         byteArr[byteArr.length] = 0x00;
         let byteArrLen = byteArr.length;
         let byteArrIndex = 0;
-        for(let blockIndex=0x04;blockIndex<0x07;blockIndex++){
+        for(let blockIndex=0x04;blockIndex<0x0B;blockIndex++){
+            //跳过0x07，因为这个是密码块，不能存储用户数据
+            if (blockIndex==0x07){
+                continue;
+            }
             let oneBlock: number[]=[]
             let i=0;
             for(;i<16;i++){
                 if (byteArrIndex >= byteArrLen){
-                    break;
+                    break; //数据已经写完了，必用再继续了
                 }
                 oneBlock[i] = byteArr[byteArrIndex++];
             }
@@ -243,7 +231,11 @@ namespace NFC {
      */
     function getStringFromCard():string{
         let byteArr: number[] = []
-        for (let blockIndex = 0x04; blockIndex < 0x07; blockIndex++) {
+        for (let blockIndex = 0x04; blockIndex < 0x0B; blockIndex++) {
+            //跳过0x07，因为这个是密码块，不能存储用户数据
+            if (blockIndex == 0x07) {
+                continue;
+            }
             let oneBlock = readOneBlock(blockIndex);
             let oneBlockLen = oneBlock.length;
             if(oneBlockLen>0){
@@ -301,13 +293,13 @@ namespace NFC {
      */
     function fillInCheckSum(dataArr: number[]): void{
         let checkSum = 0;
-        let dataArrLen = dataArr.length;
-        for (let i = 0; i < dataArrLen-1;i++){
+        let dataArrLen_1 = dataArr.length-1;
+        for (let i = 0; i < dataArrLen_1;i++){
             checkSum ^= dataArr[i];
         }
         checkSum = ~checkSum;
 
-        dataArr[dataArrLen-1] = checkSum;
+        dataArr[dataArrLen_1] = checkSum;
     }
 
 
